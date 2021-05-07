bad68da396c9beaaa749afecfcf1919f4c9e3076
forward11 <1101628423@qq.com>
2021-05-07 10:42:50 +0800

    get_date

--- FullyConnectedPredRain/wsl/data_process/date_txt.py
+++ FullyConnectedPredRain/wsl/data_process/date_txt.py
@@ -1,13 +1,15 @@
 import numpy as np
 import pandas as pd
 
-
-
-txt = np.loadtxt("C:\\Users\\11016\\Desktop\\landa_data\\2020\\12\\Data.txt")
-df = pd.DataFrame(txt)
-df.columns= ['year','month','day','hour','donnotcare']
-df=df[['year','month','day','hour']]
-#df["year"]=df.apply(lambda x:x+2000,axis=1)
-periods = pd.PeriodIndex(year=df["year"], month=df["month"], day=df["day"], hour=df['hour'], freq="H")
-df=df.set_index(periods)
-df.to_csv('C:\\Users\\11016\\Desktop\\landa_data\\2020\\12\\Date.csv')
+months = ['01','02','04','05','06','07','08','09','10','11','12']
+for month in months:
+    s1 = "C:\\Users\\11016\\Desktop\\landa_data\\2019\\" + str(month) + "\\Date.txt"
+    s2 = "C:\\Users\\11016\\Desktop\\landa_data\\2019\\" + str(month) + "\\Date.csv"
+    txt = np.loadtxt(s1)
+    df = pd.DataFrame(txt)
+    df.columns= ['year','month','day','hour','donnotcare']
+    df=df[['year','month','day','hour']]
+    #df["year"]=df.apply(lambda x:x+2000,axis=1)
+    periods = pd.PeriodIndex(year=df["year"], month=df["month"], day=df["day"], hour=df['hour'], freq="H")
+    df=df.set_index(periods)
+    df.to_csv(s2)
--- FullyConnectedPredRain/wsl/data_process/get_traValTes.py
+++ FullyConnectedPredRain/wsl/data_process/get_traValTes.py
@@ -1,11 +1,15 @@
 import os
 import pandas as pd
 
-path = "E:\\rainfall\\FullyConnectedPredRain\\wsl\\data\\yongchang\\2020"
-files = os.listdir(path)
-df_temp = []
-for file in files:
-    df = pd.read_csv(os.path.join(path,file))
-    df_temp.append(df)
-df_final = pd.concat(df_temp)
-df_final.to_csv("E:\\rainfall\\FullyConnectedPredRain\\wsl\\data\\yongchang\\2020_yc.csv")
+places = ['wsl','wuwei','yongchang']
+for place in places:
+    dirs = ['test','val','train']
+    for dir in dirs:
+        path = "E:\\rainfall\\FullyConnectedPredRain\\wsl\\data\\"+ place+ "\\" +dir
+        files = os.listdir(path)
+        df_temp = []
+        for file in files:
+            df = pd.read_csv(os.path.join(path,file))
+            df_temp.append(df)
+        df_final = pd.concat(df_temp)
+        df_final.to_csv("E:\\rainfall\\FullyConnectedPredRain\\wsl\\data\\"+ place + "\\"+dir+".csv")
