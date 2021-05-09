0ebc2d30f7b9ea31b016b755ea81f2b5c7f117d4
forward11 <1101628423@qq.com>
2021-05-09 17:17:07 +0800

    19年上半年作为验证，结果改善，数据多的话会更好

---
+++ FullyConnectedPredRain/wsl_evaluation/data_process/get_TraValTest.py
@@ -0,0 +1,15 @@
+import os
+import pandas as pd
+
+places = ['wsl','wuwei','yongchang']
+for place in places:
+    dirs = ['test','val','train']
+    for dir in dirs:
+        path = "E:\\rainfall\\FullyConnectedPredRain\\wsl_evaluation\\data\\"+ place+ "\\" +dir
+        files = os.listdir(path)
+        df_temp = []
+        for file in files:
+            df = pd.read_csv(os.path.join(path,file))
+            df_temp.append(df)
+        df_final = pd.concat(df_temp)
+        df_final.to_csv("E:\\rainfall\\FullyConnectedPredRain\\wsl_evaluation\\data\\"+ place + "\\"+dir+"_"+place+".csv",index=False)
